932c12d0e8f03751fec222309655d5af2db374ab
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-25 16:46:16 -0300

    set location, add question and format of average printout

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers will you enter? ");
         int quantityNumber = sc.nextInt();
         double[] values = new double[quantityNumber];
         double sum = 0;
-        System.out.print("How many numbers will you enter? ");
         for(int i = 0; i < values.length; i++){
+            System.out.print("enter value: ");
             values[i] = sc.nextDouble();
             sum += values[i];
         }
@@ -18,6 +21,7 @@ public class Main {
         }
         System.out.print("\n");
         System.out.println("Sum: " + sum);
+        System.out.printf("Average: %.2f", average);
         sc.close();
     }
 }
