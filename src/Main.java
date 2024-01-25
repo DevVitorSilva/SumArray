88ef731d898f0d0d6433cd7b7c0e0aaa6bc4b5c1
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-25 16:31:56 -0300

    values ​​listed and sum printed

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,18 @@ public class Main {
         int quantityNumber = sc.nextInt();
         double[] values = new double[quantityNumber];
         double sum = 0;
+        System.out.print("How many numbers will you enter? ");
         for(int i = 0; i < values.length; i++){
             values[i] = sc.nextDouble();
             sum += values[i];
         }
         double average = sum / values.length;
+        System.out.print("Values: ");
+        for(int i = 0; i < values.length; i++){
+            System.out.print(" " + values[i]);
+        }
+        System.out.print("\n");
+        System.out.println("Sum: " + sum);
         sc.close();
     }
 }
