3db6fa5d0e2d5dbc17e8e133abd7ab455b1fffb0
DevVitorSilva <vitor.silva001080@gmail.com>
2024-01-25 16:17:54 -0300

    created inputs and variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int quantityNumber = sc.nextInt();
+        double[] values = new double[quantityNumber];
+        double sum = 0;
+        double avarage = 0;
+        for(int i = 0; i < values.length; i++){
+            values[i] = sc.nextDouble();
+        }
+        sc.close();
     }
 }
